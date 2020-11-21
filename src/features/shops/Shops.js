7f9cfc81d3d77bb8a9f4b8564e5950d71761fc9a
Michal Szczepanski <lunnier@gmail.com>
2020-11-21 17:29:48 +0100

    Shops ToggleButtonGroup added

--- src/features/shops/Shops.js
+++ src/features/shops/Shops.js
@@ -2,11 +2,33 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import styles from './Shops.module.css'
 import { selectSubmitedList, selectExpense } from '../expense/expenseSlice';
+import { ToggleButtonGroup, ToggleButton } from 'react-bootstrap'
+import { selectShopsList } from '../categories/categoriesSlice';
 
 export function Shops() {
 
-    const submitedList = useSelector(selectSubmitedList)
-    const total = useSelector(selectExpense)
+    const submitedList = useSelector(selectSubmitedList);
+    const total = useSelector(selectExpense);
+
+    const ToggleButtonGroupControled = () => {
+        const shopList = useSelector(selectShopsList);
+        const [value, setValue] = useState([]);
+
+        const handleChange = (val) => setValue(val);
+
+        const shops = shopList.map((shop, index) => ((
+            <ToggleButton className={styles.toggleBtn} variant='outline-success' key={index + shop.name} value={shop.name}>
+                {shop.name}
+            </ToggleButton>
+        )))
+        return (
+            <ToggleButtonGroup type="checkbox" value={value} onChange={handleChange}>
+                {shops}
+            </ToggleButtonGroup>
+        );
+    }
+
+
 
     const summary = submitedList.map((element, index) => {
         let name = element.name.toLowerCase()
@@ -21,6 +43,7 @@ export function Shops() {
         }
     })
 
+
     return (
         <div className={styles.shops}>
             <p>Podsumowanie:</p>
@@ -28,7 +51,7 @@ export function Shops() {
             <ul className={styles.summaryList}>
                 {summary}
             </ul>
-
+            <ToggleButtonGroupControled />
         </div>
     )
 }
