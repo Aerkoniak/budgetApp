18a609feb6d873eafaa017a7f17c8984729fd482
Michal Szczepanski <lunnier@gmail.com>
2020-11-23 18:35:43 +0100

    history reducer set

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 // import counterReducer from '../features/counter/counterSlice';
 import expenseReducer from '../features/expense/expenseSlice';
 import categoriesReducer from '../features/categories/categoriesSlice';
+import historyReducer from '../features/history/historySlice';
 
 export default configureStore({
   reducer: {
     expense: expenseReducer,
-    categories: categoriesReducer
+    categories: categoriesReducer,
+    history: historyReducer
   },
 });
